f427b67c22a5a47e8400c915fe85ca6a32b07e0d
AbdullahAli2005 <smabd7409@gmail.com>
2026-03-13 17:31:44 +0500

    training loop and evaluating model

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import torch
 from torch.utils.data import Dataset, DataLoader
-from transformers import BertTokenizer, BertModel, AdamW
+from transformers import BertTokenizer, BertModel
+from torch.optim import AdamW
 
-# you can replace essay.csv with your own dataset (make sure the fields match)
-df = pd.read_csv('essay.csv')
+# you can replace essays.csv with your own dataset (make sure the fields match)
+df = pd.read_csv('essays.csv')
 print("Dataset Sample:")
 print(df.head())
 
@@ -52,7 +53,7 @@ train_texts, val_texts, train_scores, val_scores = train_test_split(
     df['Essay'], df['Overall'], test_size=0.2, random_state=42
 )
 
-train_dataset = EssayDataset(train_texts.toList(), train_scores.tolist(), tokenizer)
+train_dataset = EssayDataset(train_texts.tolist(), train_scores.tolist(), tokenizer)
 val_dataset = EssayDataset(val_texts.tolist(), val_scores.tolist(), tokenizer)
 
 train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True)
@@ -116,3 +117,28 @@ def evaluate(model, data_loader, loss_fn, device):
     mse = mean_squared_error(true_scores, predictions)
     r2 = r2_score(true_scores, predictions)
     return total_loss / len(data_loader), mse, r2
+
+# training loops
+epochs = 3
+for epoch in range(epochs):
+    train_loss = train(model, train_loader, loss_fn, optimizer, device)
+    val_loss, mse, r2 = evaluate(model, val_loader, loss_fn, device)
+    print(f'Epoch {epoch+1}, Train Loss: {train_loss:.4f}, Val loss: {val_loss:.4f}, MSE: {mse:.4f}, R2: {r2:.4f}')
+
+test_text = "The essay provided insightful analysis of the topic with well-structured arguments."
+encoding = tokenizer.encode_plus(test_text, add_special_tokens=True, max_length=512, padding='max_length',return_tensors='pt')
+input_ids = encoding['input_ids'].to(device)
+attention_mask = encoding['attention_mask'].to(device)
+
+model.eval()
+
+with torch.no_grad():
+    predicted_score = model(input_ids, attention_mask).item()
+print(f"Predicted Score: {predicted_score:.2f}")
+
+losses = {'Epoch': [1,2,3], 'Train Loss': [0.3, 0.2, 0.1], 'Val Loss': [0.35, 0.25, 0.15]}
+loss_df = pd.DataFrame(losses)
+sns.lineplot(data=loss_df, x='Epoch', y='Train Loss', label='Train Loss')
+sns.lineplot(data=loss_df, x='Epoch', y='Val Loss', label='Val Loss')
+plt.title('Training and Validation Loss')
+plt.show()
